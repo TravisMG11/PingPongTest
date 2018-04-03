74f0d1767d939a08f1f1f276f2111204c92f6caf
Travis <TravisMG11@gmail.com>
2018-04-02 21:51:51 -0400

    Add Game Creation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,38 @@
 class GamesController < ApplicationController
   def new
-    @game = Game.new
     @opponent_options = User.where.not(id: current_user.id).collect { |m| [m.email, m.id] }
   end
 
   def create
-    debugger
+    Game.create(game_attributes)
+    redirect_to :new_game
+  end
+
+  def game_attributes
+    if game_params[:your_score].to_i > game_params[:their_score].to_i
+      {
+        winner_id: current_user.id,
+        loser_id: game_params[:opponent_id],
+        winner_score: game_params[:your_score].to_i,
+        loser_score: game_params[:their_score].to_i,
+        date: game_params[:date_played]
+      }
+    else
+      {
+        winner_id: game_params[:opponent_id],
+        loser_id: current_user.id,
+        winner_score: game_params[:their_score].to_i,
+        loser_score: game_params[:your_score].to_i,
+        date: game_params[:date_played]
+      }
+    end
   end
 
   private
 
   def game_params
     params.require(:game)
-          .permit(:opponent,
+          .permit(:opponent_id,
                   :your_score,
                   :their_score,
                   :date_played
